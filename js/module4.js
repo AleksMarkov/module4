d1d7f5965da75702dffd6cd1ce8dcd754228833f
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2023-10-11 22:49:18 -0400

    Update module4.js

--- js/module4.js
+++ js/module4.js
@@ -278,3 +278,106 @@
 // );
 // console.log(allGenres);
 // console.log(uniqueGenres);
+// 21
+// const books = [
+// {
+// title: "The Last Kingdom",
+// author: "Bernard Cornwell",
+// rating: 8.38,
+// },
+// {
+// title: "Beside Still Waters",
+// author: "Robert Sheckley",
+// rating: 8.51,
+// },
+// {
+// title: "The Dream of a Ridiculous Man",
+// author: "Fyodor Dostoevsky",
+// rating: 7.75,
+// },
+// { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+// { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+//
+// const MIN_RATING = 8;
+// const AUTHOR = "Bernard Cornwell";
+// Change code below this line
+
+// const topRatedBooks = books.filter(({ rating }) => rating >= MIN_RATING);
+// const booksByAuthor = books.filter((21{ author }) => author === AUTHOR);
+// console.log(topRatedBooks);21
+// console.log(booksByAuthor);21
+// 21
+// Change code below this line
+const getUsersWithEyeColor = (users, color) => {
+  return users.filter(({ eyeColor }) => users.eyeColor === color);
+};
+// Change code above this line
+console.log(
+  getUsersWithEyeColor([
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      eyeColor: "blue",
+      friends: ["Sharron Pace"],
+      isActive: false,
+      balance: 2811,
+      gender: "male",
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      eyeColor: "blue",
+      friends: ["Briana Decker", "Sharron Pace"],
+      isActive: true,
+      balance: 3821,
+      gender: "female",
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      eyeColor: "green",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      isActive: false,
+      balance: 3793,
+      gender: "male",
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      eyeColor: "green",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      isActive: true,
+      balance: 2278,
+      gender: "female",
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      eyeColor: "blue",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      isActive: true,
+      balance: 3951,
+      gender: "male",
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      eyeColor: "brown",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      isActive: false,
+      balance: 1498,
+      gender: "male",
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      eyeColor: "brown",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      isActive: true,
+      balance: 2764,
+      gender: "female",
+    },
+  ]),
+  "blue"
+);
